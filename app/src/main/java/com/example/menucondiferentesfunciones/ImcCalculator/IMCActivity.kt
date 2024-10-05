f7c4ad1da589c853c7816199908f855128a12570
uriarteprom <programmeruriarte@gmail.com>
2024-10-04 23:55:12 -0500

    terminada app IMC

--- app/src/main/java/com/example/menucondiferentesfunciones/ImcCalculator/IMCActivity.kt
+++ app/src/main/java/com/example/menucondiferentesfunciones/ImcCalculator/IMCActivity.kt
@@ -1,6 +1,11 @@
 package com.example.menucondiferentesfunciones.ImcCalculator
 
+import android.content.Intent
+import android.icu.text.DecimalFormat
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
@@ -9,15 +14,33 @@ import androidx.core.view.KeyEventDispatcher.Component
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.menucondiferentesfunciones.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.slider.RangeSlider
 
 class IMCActivity:AppCompatActivity() {
 
     private var isMaleSelected:Boolean=true
     private var isFemaleSelected:Boolean=false
+    private var currentWeight:Int=70
+    private var currentAge:Int=20
+    private var currentHeight:Int=120
 
     private lateinit var cardMale:CardView
     private lateinit var cardFemale:CardView
-            
+    private lateinit var tvHeight:TextView
+    private lateinit var rsHeight:RangeSlider
+    private lateinit var btnDownWeight:FloatingActionButton
+    private lateinit var btnUpWeight:FloatingActionButton
+    private lateinit var tvWeight:TextView
+    private lateinit var btnDownAge:FloatingActionButton
+    private lateinit var btnUpAge:FloatingActionButton
+    private lateinit var tvAge:TextView
+    private lateinit var btnCalculate:Button
+
+    companion object{
+        const val IMC_KEY= "IMC_RESULT"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -35,6 +58,15 @@ class IMCActivity:AppCompatActivity() {
     private fun initComponets() {
         cardMale=findViewById(R.id.cardMale)
         cardFemale=findViewById(R.id.cardFemale)
+        tvHeight=findViewById(R.id.tvHeight)
+        rsHeight=findViewById(R.id.rsHeight)
+        btnDownWeight=findViewById(R.id.btnDownWeight)
+        btnUpWeight=findViewById(R.id.btnUpWeight)
+        tvWeight=findViewById(R.id.tvWeight)
+        btnDownAge=findViewById(R.id.btnDownAge)
+        btnUpAge=findViewById(R.id.btnUpAge)
+        tvAge=findViewById(R.id.tvAge)
+        btnCalculate=findViewById(R.id.btnCalculate)
     }
     private fun initListeners() {
         cardMale.setOnClickListener{
@@ -45,6 +77,51 @@ class IMCActivity:AppCompatActivity() {
             changeGender()
             setGenderColor()
         }
+        rsHeight.addOnChangeListener { _, value, _ ->
+            val df=DecimalFormat("#.##")
+            currentHeight = df.format(value).toInt()
+            tvHeight.text = "$currentHeight CM"
+        }
+        btnUpWeight.setOnClickListener {
+            currentWeight += 1
+            setWeight()
+        }
+        btnDownWeight.setOnClickListener {
+            currentWeight -= 1
+            setWeight()
+        }
+        btnUpAge.setOnClickListener {
+            currentAge += 1
+            setAge()
+        }
+        btnDownAge.setOnClickListener {
+            currentAge -= 1
+            setAge()
+        }
+        btnCalculate.setOnClickListener {
+            val result = calculateIMC()
+            navigateToResult(result)
+        }
+    }
+
+    private fun navigateToResult(result: Double) {
+        val intent=Intent(this, ResultIMCActivity::class.java)
+        intent.putExtra(IMC_KEY, result)
+        startActivity(intent)
+    }
+
+    private fun calculateIMC(): Double {
+        val df=DecimalFormat("#.##")
+        val imc = currentWeight/(currentHeight.toDouble()/100 * currentHeight.toDouble()/100)
+        return df.format(imc).toDouble()
+
+    }
+
+    private fun setWeight(){
+        tvWeight.text = currentWeight.toString()
+    }
+    private fun setAge(){
+        tvAge.text = currentAge.toString()
     }
 
     private fun changeGender(){
@@ -68,5 +145,7 @@ class IMCActivity:AppCompatActivity() {
 
     private fun initUI() {
         setGenderColor()
+        setWeight()
+        setAge()
     }
 }
---
+++ app/src/main/java/com/example/menucondiferentesfunciones/ImcCalculator/ResultIMCActivity.kt
@@ -0,0 +1,71 @@
+package com.example.menucondiferentesfunciones.ImcCalculator
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.menucondiferentesfunciones.ImcCalculator.IMCActivity.Companion.IMC_KEY
+import com.example.menucondiferentesfunciones.R
+
+class ResultIMCActivity : AppCompatActivity() {
+
+    private lateinit var tvResult: TextView
+    private lateinit var tvIMC: TextView
+    private lateinit var tvDescription: TextView
+    private lateinit var btnRecalculat: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_result_imcactivity)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val result:Double = intent.extras?.getDouble(IMC_KEY) ?: -1.0
+        initComponents()
+        initUI(result)
+    }
+
+    private fun initListeners(){
+        btnRecalculat.setOnClickListener { onBackPressed() }
+    }
+
+    private fun initUI(result: Double) {
+        tvIMC.text= result.toString()
+        when(result){
+            in 0.00..18.50->{
+                tvResult.text = getString(R.string.tBajoPeso)
+                tvDescription.text = getString(R.string.dBajoPeso)
+            }
+            in 18.51..24.99->{
+                tvResult.text = getString(R.string.tPesoNormal)
+                tvDescription.text = getString(R.string.dPesoNormal)
+            }
+            in 25.00..29.99->{
+                tvResult.text = getString(R.string.tSobrePeso)
+                tvDescription.text = getString(R.string.dSobrePeso)
+            }
+            in 30.00..99.99->{
+                tvResult.text = getString(R.string.tObesidad)
+                tvDescription.text = getString(R.string.dObesidad)
+            }
+            else ->{
+                tvIMC.text = getString(R.string.error)
+                tvResult.text = getString(R.string.error)
+                tvDescription.text = getString(R.string.error)
+            }
+        }
+    }
+
+    private fun initComponents(){
+        tvResult=findViewById(R.id.tvResult)
+        tvIMC=findViewById(R.id.tvIMC)
+        tvDescription=findViewById(R.id.tvDescription)
+        btnRecalculat=findViewById(R.id.btnReCalculate)
+    }
+}
